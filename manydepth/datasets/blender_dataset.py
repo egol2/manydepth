0e2611f7b577137464efa0fa8454a5ce03116e6b
egol2 <egor@vt.edu>
2023-11-21 20:14:18 -0500

    started setup for custom dataset

---
+++ manydepth/datasets/blender_dataset.py
@@ -0,0 +1,63 @@
+import os
+os.environ["MKL_NUM_THREADS"] = "1"  # noqa F402
+os.environ["NUMEXPR_NUM_THREADS"] = "1"  # noqa F402
+os.environ["OMP_NUM_THREADS"] = "1"  # noqa F402
+import skimage.transform
+import numpy as np
+import PIL.Image as pil
+
+from manydepth.kitti_utils import generate_depth_map
+from .mono_dataset import MonoDataset
+
+class BlenderDataset(MonoDataset):
+    """Superclass for a generated blender dataset
+    """
+    def __init__(self, *args, **kwargs):
+        super(BlenderDataset, self).__init__(*args, **kwargs)
+
+        # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size
+        self.K = np.array([[0.58, 0, 0.5, 0],
+                           [0, 1.92, 0.5, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]], dtype=np.float32)
+
+        self.full_res_shape = (1242, 375)
+        self.side_map = {"2": 2, "3": 3, "l": 2, "r": 3}
+
+    def check_depth(self):
+        line = self.filenames[0].split()
+        scene_name = line[0]
+        frame_index = int(line[1])
+
+        velo_filename = os.path.join(
+            self.data_path,
+            scene_name,
+            "velodyne_points/data/{:010d}.bin".format(int(frame_index)))
+
+        return os.path.isfile(velo_filename)
+
+    def index_to_folder_and_frame_idx(self, index):
+        """Convert index in the dataset to a folder name, frame_idx and any other bits
+        """
+        line = self.filenames[index].split()
+        folder = line[0]
+
+        if len(line) == 3:
+            frame_index = int(line[1])
+        else:
+            frame_index = 0
+
+        if len(line) == 3:
+            side = line[2]
+        else:
+            side = None
+
+        return folder, frame_index, side
+
+    def get_color(self, folder, frame_index, side, do_flip):
+        color = self.loader(self.get_image_path(folder, frame_index, side))
+
+        if do_flip:
+            color = color.transpose(pil.FLIP_LEFT_RIGHT)
+
+        return color
